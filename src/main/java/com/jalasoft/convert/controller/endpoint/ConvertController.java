4ba37ef5d736f408a121ea0785e67aa5dae76300
MagdalenaPonce <magdalena.ponce@jalasoft.com>
2020-05-19 21:50:38 -0400

    Convert and convertMultiPartToFile methods were moved to new clasess,

--- src/main/java/com/jalasoft/convert/controller/endpoint/ConvertController.java
+++ src/main/java/com/jalasoft/convert/controller/endpoint/ConvertController.java
@@ -1,62 +1,50 @@
 package com.jalasoft.convert.controller.endpoint;
 
+import com.jalasoft.convert.model.Convert;
+import com.jalasoft.convert.model.cast.CastMultipartToFile;
+
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.io.File;
 
-import com.documents4j.api.DocumentType;
-import com.documents4j.api.IConverter;
-import com.documents4j.job.LocalConverter;
-
-import org.apache.commons.io.FilenameUtils;
 
 @RestController
-@RequestMapping("/convert")
+@RequestMapping("/v1")
 public class ConvertController {
-    private static String uploadFolder = "D://dev-fundamentals-II//uploadFiles//";
-    //FileOutputStream fileOutputStream = new FileOutputStream(source);
 
-    @PostMapping
-    public String sayHello(@RequestParam(value="name") String name,
-                           @RequestParam(value="lastName") String lname,
+    @PostMapping("/convert")
+    public String convertFile(@RequestParam(value="lang") String lang,
                            @RequestParam("file") MultipartFile inputFile) {
-        if(inputFile.isEmpty()) {
-            return "Please select a FIle and try again";
-        }
+
+        Convert convert = new Convert();
+        String uploadFolder = "filesContainer/";
+        CastMultipartToFile convertToFile = new CastMultipartToFile();
+        String outputFileName = FilenameUtils.removeExtension(inputFile.getOriginalFilename()) + ".pdf";
+
         try {
-
-            InputStream docxInputStream = new FileInputStream(convertMultiPartToFile(inputFile));
-            String outputFileName = FilenameUtils.removeExtension(inputFile.getOriginalFilename()) + ".pdf";
+            Files.createDirectories(Paths.get(uploadFolder));
+            File file = convertToFile.convertMultiPartToFile(inputFile);
             File outputFile = new File (uploadFolder + outputFileName);
+            InputStream docxInputStream = new FileInputStream(file);
             OutputStream outputStream = new FileOutputStream(outputFile);
-            IConverter converter = LocalConverter.builder().build();
-            converter.convert(docxInputStream).as(DocumentType.DOCX).to(outputStream).as(DocumentType.PDF).execute();
-            outputStream.close();
 
+            convert.convertFile(docxInputStream, outputStream);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return "Hello " + name + " " + lname + "file converted successfully to PDF";
+        return "Hello! Your " + lang + " file was converted successfully to PDF";
     }
-    private File convertMultiPartToFile(MultipartFile file ) throws IOException
-    {
-        File convFile = new File( file.getOriginalFilename() );
-        FileOutputStream fos = new FileOutputStream( convFile );
-        fos.write( file.getBytes() );
-        fos.close();
-        return convFile;
-    }
+
 }
